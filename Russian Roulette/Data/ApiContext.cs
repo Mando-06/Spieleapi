88a32c84296a330a31a4979dce5e16cf5bbbbecc
robin_sacher <110891559+robinsacher@users.noreply.github.com>
2024-02-02 10:15:11 +0100

    Add files via upload

---
+++ Russian Roulette/Data/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Russian_Roulette.Models;
+namespace Russian_Roulette.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Spieler> Spieler { get; set; }
+        
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+            
+        }
+    }
+}
