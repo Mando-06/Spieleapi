10cf1d1dbbeebea281bdd2a8bef24e631a406d02
robin_sacher <110891559+robinsacher@users.noreply.github.com>
2024-02-18 14:14:24 +0100

    Add files via upload

---
+++ Russian Roulette/Models/Revolver.cs
@@ -0,0 +1,10 @@
+﻿namespace Russian_Roulette.Models
+{
+    public class Revolver
+    {
+        public int Id { get; set; }
+
+        public int loadedChamber { get; set; }
+        public int shotChamber { get; set; }
+    }
+}
