c46e337832abcec22849ea014ff251963dc00d9f
robin_sacher <110891559+robinsacher@users.noreply.github.com>
2024-03-01 10:13:59 +0100

    Delete Spieleapi/Russian Roulette directory

--- Spieleapi/Russian Roulette/Models/Punktezahl_SchereSteinPapier.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Russian_Roulette.Models
-{
-    public class Punktezahl_SchereSteinPapier
-    {
-        public int Id { get; set; }
-        public int PunkteSpieler { get; set; }
-        public int PunkteBot { get; set; }
-    }
-}
